cf17a37e694157cbaa18307d06c9474bcedca849
SounakShome <sounakshome@gmail.com>
2025-07-10 07:33:58 +0530

    added api routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+
+import { useCounterStore } from "@/store";
 import Image from "next/image";
 
 export default function Home() {
+
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {count}
+      <div className="flex gap-4 items-center justify-center row-start-1">
+        <button
+          className="p-4 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a]" onClick={increment}>Increment</button>
+        <button
+          className="p-4 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a]"
+          onClick={decrement}
+        >
+        Decrement
+        </button>
+      </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
